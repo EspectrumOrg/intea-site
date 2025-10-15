54cd38a2b7fd89e3378c387fddb81882cb92734d
NicolasIqui <suicunelindo@gmail.com>
2025-10-15 19:46:27 -0300

    removendo o chat.blade uma view inutil

--- resources/views/feed/chats/template/chat_layout.blade.php
+++ resources/views/feed/chats/template/chat_layout.blade.php
@@ -5,10 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Chat')</title>
     <link rel="stylesheet" href="{{ asset('assets/css/style2.css') }}">
+    
     <script src="https://js.pusher.com/7.2/pusher.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
 </head>
 <body>
+
+
+
     <div class="container">
         @yield('main')
     </div>
