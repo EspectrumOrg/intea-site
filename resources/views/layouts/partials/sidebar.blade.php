bf6677ee8bb6c1b1f9d1dea9e66b2cbaa9497dd3
k112p1 <142914160+K112p1Esc@users.noreply.github.com>
2025-11-18 21:05:33 -0300

    Update icone

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,8 +1,10 @@
 <!-- icons and style -->
 <link rel="stylesheet" href="{{ url('assets/css/layout/sidebar.css') }}">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+<link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
-<div class="content {{ Auth::check() && Auth::user()->tema_preferencia == 'monocromatico' ? 'sidebar-monochrome' : '' }}">
+<div
+    class="content {{ Auth::check() && Auth::user()->tema_preferencia == 'monocromatico' ? 'sidebar-monochrome' : '' }}">
 
     <!-- Logo -->
     <div class="logo">
@@ -14,13 +16,8 @@
     <!-- User info -->
     <div class="info dropdown-container" id="userDropdown">
         <a href="#">
-            <img
-                src="{{ Auth::user()->foto ? url('storage/' . Auth::user()->foto) : asset('assets/images/logos/contas/user.png') }}"
-                alt="foto de perfil"
-                style="border-radius: 50%;"
-                width="40"
-                height="40"
-                loading="lazy">
+            <img src="{{ Auth::user()->foto ? url('storage/' . Auth::user()->foto) : asset('assets/images/logos/contas/user.png') }}"
+                alt="foto de perfil" style="border-radius: 50%;" width="40" height="40" loading="lazy">
         </a>
         <div class="text">
             <h5>{{ Auth::user()->apelido }}</h5>
@@ -39,15 +36,16 @@
             </li>
         </ul>
     </div>
-@php  
-    $totalNotificacoes = \App\Models\Notificacao::where('alvo_id', auth()->id())
-        ->whereIn('tipo', ['seguindo_voce', 'seguir','solicitacao_aceita', 'seguir'])
+    @php  
+        $totalNotificacoes = \App\Models\Notificacao::where('alvo_id', auth()->id())
+        ->whereIn('tipo', ['seguindo_voce', 'seguir', 'solicitacao_aceita', 'seguir'])
         ->count();
-@endphp
+    @endphp
 
     <!-- Links sidebar-->
     <div class="links">
-        <a href="{{ route('post.index') }}" class="nav-link {{ request()->routeIs('post.index') || request()->routeIs('post.seguindo') ? 'active' : '' }}"
+        <a href="{{ route('post.index') }}"
+            class="nav-link {{ request()->routeIs('post.index') || request()->routeIs('post.seguindo') ? 'active' : '' }}"
             id="home">
             <span id="simbolo-home" class="material-symbols-outlined">home</span>
             <h1>Home</h1>
@@ -61,7 +59,8 @@
         </a>
         -->
 
-        <a href="{{ route('chat.dashboard') }}" class="nav-link {{ request()->routeIs('chat.dashboard') ? 'active' : '' }}" id="message">
+        <a href="{{ route('chat.dashboard') }}"
+            class="nav-link {{ request()->routeIs('chat.dashboard') ? 'active' : '' }}" id="message">
             <span id="simbolo-mensagens" class="material-symbols-outlined">mail</span>
             <h1>Mensagens</h1>
         </a>
@@ -72,29 +71,28 @@
             <h1>Perfil</h1>
         </a>
 
-<a href="{{ route('notificacao.index') }}" 
-   class="nav-link {{ request()->routeIs('notificacao.index') ? 'active' : '' }}"
-   id="config">
+        <a href="{{ route('notificacao.index') }}"
+            class="nav-link {{ request()->routeIs('notificacao.index') ? 'active' : '' }}" id="config">
 
-    <span id="simbolo-grupos" class="material-symbols-outlined">group</span>
+            <span class="material-symbols-outlined"> notifications </span>
 
-    <h1 style="display: flex; align-items: center; gap: 6px;">
-        Notificações
+            <h1 style="display: flex; align-items: center; gap: 6px;">
+                Notificações
 
-        @if($totalNotificacoes > 0)
-            <span style="
-                background: #14b814;
-                color: white;
-                padding: 2px 8px;
-                border-radius: 12px;
-                font-size: 12px;
-                font-weight: bold;
-            ">
-                {{ $totalNotificacoes }}
-            </span>
-        @endif
-    </h1>
-</a>
+                @if($totalNotificacoes > 0)
+                        <span style="
+                        background: #14b814;
+                        color: white;
+                        padding: 2px 8px;
+                        border-radius: 12px;
+                        font-size: 12px;
+                        font-weight: bold;
+                    ">
+                            {{ $totalNotificacoes }}
+                        </span>
+                @endif
+            </h1>
+        </a>
 
         <a href="{{ route('configuracao.config') }}"
             class="nav-link {{ request()->routeIs('configuracao.config') ? 'active' : '' }}">
@@ -105,14 +103,15 @@
 
 
         @can("visualizar-admin")
-        <a href="{{ route('dashboard.index') }}"
-            class="nav-link {{ request()->routeIs('dashboard.index') ? 'active' : '' }}">
-            <span class="material-symbols-outlined">manage_accounts</span>
-            <h1>Administração</h1>
-        </a>
+            <a href="{{ route('dashboard.index') }}"
+                class="nav-link {{ request()->routeIs('dashboard.index') ? 'active' : '' }}">
+                <span class="material-symbols-outlined">manage_accounts</span>
+                <h1>Administração</h1>
+            </a>
         @endcan
         @can("visualizar-responsavel")
-            <a href="{{ route('responsavel.painel') }}" class="nav-link {{ request()->routeIs('responsavel.painel') ? 'active' : '' }}">
+            <a href="{{ route('responsavel.painel') }}"
+                class="nav-link {{ request()->routeIs('responsavel.painel') ? 'active' : '' }}">
                 <span id="simbolo-responsavel" class="material-symbols-outlined">supervisor_account</span>
                 <h1>Responsáveis</h1>
             </a>
